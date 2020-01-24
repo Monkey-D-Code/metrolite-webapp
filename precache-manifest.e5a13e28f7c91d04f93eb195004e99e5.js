e6a0f6e7b2d5228c82699e669b4d3c672681fc13
Monkey-D-Code <theartofdeception08@gmail.com>
2020-01-24 16:20:20 +0530

    pages compatible

---
+++ precache-manifest.e5a13e28f7c91d04f93eb195004e99e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907e03f5e97bfb5b76f2b7c78c9d444d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d81832ef5a089547bd4",
+    "url": "/static/css/main.18178332.chunk.css"
+  },
+  {
+    "revision": "b80f51242cbf28469333",
+    "url": "/static/js/2.05cc257b.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.05cc257b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d81832ef5a089547bd4",
+    "url": "/static/js/main.4a9b6380.chunk.js"
+  },
+  {
+    "revision": "b0aea18ec71530abcfd6",
+    "url": "/static/js/runtime-main.9508d96c.js"
+  }
+]);
